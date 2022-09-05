77d6a6d8b1ed7d7058ff99daf9500d554eadc37d
unknown <rshina@hotmail.com>
2022-09-05 15:52:39 -0300

    exercicios add

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -5,3 +5,26 @@ it('testa a página da política de privavidade de forma independente',function(
         .should('be.visible')    
 })
 
+/*
+Obs:Exemplo de função time aplicada para repetição de teste para confirmar sua estabilidade.
+
+Cypress._.times(5, function() {
+
+    it('testa a página da política de privavidade de forma independente',function(){
+    
+        cy.visit('./src/privacy.html')
+        cy.contains('Talking About Testing')
+            .should('be.visible')    
+    })  */
+
+
+
+
+
+
+
+
+
+
+
+
